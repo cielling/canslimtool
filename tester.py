8bfa42bbb778e7ca360bde69460abb0d9bb91ca8
Dr. M0hawk <carola.ellinger@gmail.com>
2018-11-02 15:28:57 -0600

    Print out what is actually being tested.

--- tester.py
+++ tester.py
@@ -8,23 +8,46 @@ all10Ks = pd.read_csv("TestData\\nvda_all_10ks.csv", parse_dates=['date'], dtype
 all10Qs = pd.read_csv("TestData\\nvda_all_10qs.csv", parse_dates=['date'], dtype={'cik':str, 'conm':str, 'type':str,'path':str})
 
 NvdaCanslimParams= CanslimParams("NVDA", all10Qs, all10Ks)
-NvdaCanslimParams.loadData()
-NvdaCanslimParams.getEpsQuarter(0)
-NvdaCanslimParams.getEpsAnnual(-1)
-NvdaCanslimParams.getSalesQuarter(-2)
-NvdaCanslimParams.getSalesAnnual(-2)
-NvdaCanslimParams.getRoeCurrent()
-NvdaCanslimParams.getEpsGrowthQuarter(-3, -4)
-NvdaCanslimParams.getEpsGrowthAnnual(0, -1)
-NvdaCanslimParams.getStabilityOfEpsGrowth(4)
-NvdaCanslimParams.getEpsGrowthAcceleration(4)
+## Load the data, and proceed if successful.
+if NvdaCanslimParams.loadData():
+    ## Test all the EPS stuff
+    ## Test the last four quarters to cover the case where the 10-K was filed instead of the 10-Q.
+    print("Getting EPS for Q0:")
+    print(NvdaCanslimParams.getEpsQuarter(0))
+    print("Getting EPS for Q-1:")
+    print(NvdaCanslimParams.getEpsQuarter(-1))
+    print("Getting EPS for Q-2:")
+    print(NvdaCanslimParams.getEpsQuarter(-2))
+    print("Getting EPS for Q-3:")
+    print(NvdaCanslimParams.getEpsQuarter(-3))
+    ## Test the last two years of EPS
+    print("Getting EPS for Y0:")
+    print(NvdaCanslimParams.getEpsAnnual(0))
+    print("Getting EPS for Y-1:")
+    print(NvdaCanslimParams.getEpsAnnual(-1))
+    print("Getting EPS growth for Q-3 to Q-4:")
+    print(NvdaCanslimParams.getEpsGrowthQuarter(-3, -4))
+    print("Getting EPS for Y0 to Y-1:")
+    print(NvdaCanslimParams.getEpsGrowthAnnual(0, -1))
+    print("Getting stability of EPS:")
+    print(NvdaCanslimParams.getStabilityOfEpsGrowth(4))
+    print("Getting EPS growth acceleration:")
+    print(NvdaCanslimParams.getEpsGrowthAcceleration(4))
 
-Canslim = {}
-Canslim['Eps_current_Q_per_same_Q_prior_year'] = -99
-Canslim['Sales_current_Q_per_prior_Q'] = -99
-Canslim['Sales_growth_accel_last_3_Q'] = -99
-Canslim['Eps_growth_accel_last_3_Q'] = -99
-Canslim['Stability_of_Q_eps_growth_last_3_years'] = -99
-Canslim['Excellency_of_eps_increase'] = -99
-Canslim['Num_Q_with_eps_growth_deceleration'] = -99
-Canslim['Num_years_annual_eps_increasing_last_3_years'] = -99
+    ## Test the Sales stuff
+    print("Getting sales for Q-2:")
+    print(NvdaCanslimParams.getSalesQuarter(-2))
+    print("Getting EPS for Y-1:")
+    print(NvdaCanslimParams.getSalesAnnual(-1))
+    print("Getting sales growth between Q0 and Q-2:")
+    print(NvdaCanslimParams.getSalesGrowth(0, -2))
+    print("Getting sales growth acceleration:")
+    print(NvdaCanslimParams.getSalesGrowthAcceleration(4))
+
+    ## Test the ROE
+    print("Getting current ROE:")
+    print(NvdaCanslimParams.getRoeCurrent())
+
+    ## Test the auxiliary functions
+
+del NvdaCanslimParams
