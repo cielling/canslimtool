f8a10dfadb421f9847ebf5327d46949eff0c6b75
Carola I. Ellinger <carola.ellinger@gmail.com>
2019-10-25 12:32:31 -0600

    Updating MMM tester to test for EPS growth stability.
    And it also now contains all the Sec-filing data for reference.

--- tester_mmm.py
+++ tester_mmm.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import pandas as pd
 from bs4 import BeautifulSoup as BSoup
+from CanslimParams import CanslimParams
+import numpy as np
+from datetime import date, timedelta
 
 ## "current date" = 2019 Q1
-
-from CanslimParams import CanslimParams
 
 def areEqual(expect, val, eps = 0.01):
     print("Expected: ", expect, " actual: ", val)
@@ -16,6 +17,19 @@ def areEqual(expect, val, eps = 0.01):
         print(be)
 
 ticker = "MMM"
+
+## Data from SEC-filings
+eps = np.array([1.54, 2.3, 2.64, 3.14, 1.01, 0.88, 2.39, 2.65, 2.21, 1.92, 2.2, 2.13, 2.1, 1.69, 2.09, 2.06, 1.88])
+dates=np.array([date(2019, 3, 31), date(2018, 12, 31), date(2018, 9, 30), date(2018, 6, 30), date(2018, 3, 31), date(2017, 12, 31), date(2017, 9, 30), date(2017, 6, 30), date(2017, 3, 31), date(2016, 12, 31), date(2016, 9, 30), date(2016, 6, 30), date(2016, 3, 31), date(2015, 12, 31), date(2015, 9, 30), date(2015, 6, 30), date(2015, 3, 31)])
+sales=np.array([7863000000, 7945000000, 8152000000, 8390000000, 8278000000, 8008000000, 8172000000, 7810000000, 7685000000, 7329000000, 7709000000, 7662000000, 7409000000, 7298000000, 7712000000, 7686000000, 7578000000])
+se=np.array([9703000000, 10407000000, 10248000000, 10365000000, 10977000000, 11672000000, 12146000000, 11591000000, 10989000000, 11316000000, 12002000000, 11894000000, 11733000000, 12484000000, 12186000000, 13093000000, 13917000000])
+ni=np.array([891000000, 1361000000, 1543000000, 1857000000, 602000000, 534000000, 1429000000, 1583000000, 1323000000, 1163000000, 1329000000, 1291000000, 1275000000, 1041000000, 1296000000, 1303000000, 1201000000])
+delta=dates-dates[0]
+l=[]
+for d in delta:
+    l.append(d.days)
+days=np.array(l)
+
 
 print("Testing ticker {:s}".format(ticker))
 
@@ -95,6 +109,11 @@ if canslim.loadData("TestData"):
     print("Getting current ROE (TTM):")
     expect = (891000000 + 5363000000 - 602000000) / (9703000000 + 10407000000 + 10248000000 + 10365000000) * 4 * 100.0
     val = canslim.getRoeTTM()
+    areEqual(expect, val)
+    
+    print("Getting stability of EPS - last 16 Q:")
+    expect = 3.39633
+    val = canslim.getStabilityOfEpsGrowth(16)
     areEqual(expect, val)
     
 
